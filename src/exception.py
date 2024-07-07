042691ac7c5213d939ed291d455169886853ba28
novice1205 <parthraheja1205@gmail.com>
2024-07-07 18:24:32 +0530

    Exception and Logs

---
+++ src/exception.py
@@ -0,0 +1,27 @@
+import logging
+import sys
+
+def error_message_detail(error,error_info:sys):
+    _,_,exc_tb = error_info.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    error_message = f"Error occured in python script name [{file_name}] line number [{exc_tb.tb_lineno}] error message [{str(error)}]"
+
+    return error_message
+
+
+class CustomException(Exception):
+    def __init__(self,error_message,error_info:sys):
+        super().__init__(error_message)
+        self.error_message = error_message_detail(error_message,error_info=error_info)
+
+    def __str__(self):
+        return self.error_message
+    
+if __name__ == "__main__":
+
+    try:
+        a = 1/0
+    except Exception as e:
+        logging.info("Division by Zero Error")
+        logging.error("An error occured",exc_info=True)
+        raise CustomException(e,sys)
